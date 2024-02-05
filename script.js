b18f508a2d1330db360864e314aa88ab37879068
Roshan Ali <147090241+roshanali007@users.noreply.github.com>
2024-02-05 11:48:43 -0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+function bclick(val)
+{
+    document.getElementById("screen").value+=val
+}
+function clearscreen(val)
+{
+    document.getElementById("screen").value=""
+}
+function equalclick()
+{
+    var text=document.getElementById("screen").value
+    var result=eval(text)
+    document.getElementById("screen").value=result
+}
